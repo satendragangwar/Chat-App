46d73bc44142cdb02dc32516272f9a3e091f5013
satendra gangwar <gsatendra488@gmail.com>
2024-02-13 03:39:04 +0530

    initil

---
+++ server/db/connection.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const url = 
+
+
+mongoose.connect(url, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+}).then(() => console.log('Connected to DB')).catch((e)=> console.log('Error', e))
